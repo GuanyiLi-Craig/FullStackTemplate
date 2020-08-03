c7ed9e2815990230910254bf7c06621f6d910d44
bitforcestudio <bitforce.studio@gmail.com>
2020-08-03 18:10:32 +0100

    added dataset worker project

---
+++ 4 logic/backend/dataset-worker/src/main/java/com/bitforcestudio/datasetworker/DatasetWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.bitforcestudio.datasetworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasetWorkerApplication.class, args);
+	}
+
+}
---
+++ 4 logic/backend/dataset-worker/src/test/java/com/bitforcestudio/datasetworker/DatasetWorkerApplicationTests.java
@@ -0,0 +1,5 @@
+package com.bitforcestudio.datasetworker;
+
+class DatasetWorkerApplicationTests {
+
+}
